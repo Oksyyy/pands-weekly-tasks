d4047c74e56cd64a475f0daae1af978c5bc8153b
Oksana Abrosimova <abricosova.oxi@gmail.com>
2025-04-06 08:38:56 +0100

    week 8 lecture practice

---
+++ week08/8.1.1_salaries.py
@@ -0,0 +1,11 @@
+# salaries.py
+# Author: Oksana 
+import numpy as np
+
+min_salary = 20000
+max_salary = 80000
+num_of_salaries = 10
+
+salaries = np.random.randint(min_salary, max_salary, num_of_salaries)
+
+print(salaries)
