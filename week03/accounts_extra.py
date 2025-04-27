81b030b5156ea301ec63ec536aaa6cb1ca1f64a9
Oksana Abrosimova <abricosova.oxi@gmail.com>
2025-04-27 16:29:28 +0100

    Reviewed the task, fixed gramma mistakes, added more commentary

--- week03/accounts_extra.py
+++ week03/accounts_extra.py
@@ -5,17 +5,24 @@
 # VIRSION_2 (Extra) - handles any length account number
 
 account = input('Please enter an account number: ') 
+# prompt the user to enter an account number using input() function
 
 account_len = len(account) 
-# counts number of digits in the account number 
+# use len() function to count the number of digits in the account number enterred by the user
 
 account_len_less_last4 = len(account) - 4 
-# counts number of digits in the account number less last 4 digits 
+# deduct 4 digits from the account number length
+# this is done so we can dynamically replace the first part of the account number with X'es (for the account number of any length) 
 
 masked_part = account_len_less_last4 * "X"
- # "X" times account_len_less_last4 allows to replace first part of the account (with unspecified length) with X'es to align number of X'es with the number of digits
+# "X" times account_len_less_last4 allows to replace first part of the account with X'es
+# multiplication of integer and string creates multiple copies of the same string character
 
-fomatted_account = masked_part + account[account_len - 4 : account_len]
-# add masked_part & slice the first part of the account number leaving only last 4 digits 
+fomatted_account = masked_part + account[account_len_less_last4 : account_len]
+# combine the masked_part with the last 4 digits of the account number
+# to get the last 4 digits we use slicing of the first part of the account number
+# in the slicing function the first argument is the starting index 
+# the second argument after semicolon is the ending index
 
 print(fomatted_account)
+# print formatted account back to the user 
