bde43b7ec142c653cd2e74a249491c5cdba0779e
Oksana Abrosimova <abricosova.oxi@gmail.com>
2025-04-27 17:56:47 +0100

    Improved commentary and fixed gramma mistakes

--- week05/weekday.py
+++ week05/weekday.py
@@ -2,8 +2,6 @@
 # Program that outputs whether or not today is a weekday
 # Author: Oksana Abrosimova
 
-
-
 import datetime 
 # import datetime module to work with dates as date objects
  
@@ -16,7 +14,11 @@ if today.strftime("%w") in range(1,6):
 # use range() function to set a range from 1 to 6 (6 is not included), where 1 = Monday and 5 = Friday 
     
     print('Yes, unfortunately today is a weekday.')
-    # if the current day number is foubd in the set range and satisfies the above condition, it's a weekday
+    # if the number that corresponds to the current day is found in the range from 1 (Monday) to 5 (Friday) (6 expluded), it's a weekday
+    # print a message to the user that today is a weekday
+
 else:
 # if the above condition evaluation is false, then it's a weekend
+    
     print('It is the weekend, yay!')
+    # print another message to the user that today is a weekend
