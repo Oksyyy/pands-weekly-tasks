0a490f201cb972fd5be36fd2ecb347ad899e1582
Oksana Abrosimova <abricosova.oxi@gmail.com>
2025-04-27 16:46:45 +0100

    Reviewed the task + improved commentary and variable names + added rounding to format the final amount

--- week02/bank.py
+++ week02/bank.py
@@ -1,12 +1,22 @@
 # bank.py
-# Program that reads in multiple amounts in cents and then sums them up and prints out the total amount in €
+# Program that reads in multiple amounts in cents, sums them up and prints out the total amount in €
 # Author: Oksana Abrosimova
 
-amount1 = int(input('Enter amount 1 (in cents): ')) ## int converts the enterred amount into an integer
+amount1 = int(input('Enter amount 1 (in cents): '))
 amount2 = int(input('Enter amount 2 (in cents): '))
+# use input() function to prompt a user to enter amounts in cents
+# use int() fuction to convert the enterred amount into an integer. Input is always a string
 
-newAmount1 = amount1/100 ## division by 100 converts cents into decimal points amounts
-newAmount2 = amount2/100
-finalNumber = newAmount1+newAmount2 ## addition adds 2 amounts together for the final output
+converted_amount1 = amount1/100 
+# create a new variable to format amount as required per the task description
+# division by 100 converts cents into decimal point amounts which represents euro
+converted_amount2 = amount2/100
+# convert the second amount
 
-print(f'The sum of these is €{finalNumber}') # f-string enables formating output result by combining a message & a calculated final amount
+final_amount = converted_amount1 + converted_amount2 
+# create another variable to store the final amount
+# add 2 converted amounts together for the final output
+
+print(f'The sum of these is €{final_amount:.2f}') 
+# use f-string to format the program output by passing a message & final_amount variable back to the user
+# use .2f to format the output to 2 decimal places
